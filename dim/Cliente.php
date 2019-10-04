79cf2f4c2f99697d65bcc0e92fa5b3211b7ad38c
RafaelLC31 <rafael.lc31@aluno.ifsc.edu.br>
2019-10-04 20:26:21 -0300

    Implementado o Model para o clinte e iniciada a classe da carga da dimensão cliente

---
+++ dim/Cliente.php
@@ -0,0 +1,72 @@
+<?php 
+
+namespace DIM;
+
+/**
+    * Model da entidade cliente
+    * @autor Rafael Laba da Costa
+*/ 
+
+    class Cliente{
+        public $cpf;
+    /**
+        * CPF do cliente
+        * @var string
+    */
+        public $nome;
+    /**
+        * nome do cliente
+        * @var string
+    */        
+        public $sexo;
+    /**
+        * sexo do cliente
+        * @var string
+    */
+        public $idade;
+    /**
+        * idade do cliente
+        * @var string
+    */
+        public $email;
+    /**
+        * email do cliente
+        * @var string
+    */
+        public $rua;
+    /**
+        * rua do cliente
+        * @var string
+    */
+        public $bairro;
+    /**
+        * bairro do cliente
+        * @var string
+    */
+        public $cidade;
+    /**
+        * cidade do cliente
+        * @var string
+    */
+        public $uf;
+    /**
+        * uf do cliente
+        * @var string
+    */
+    }
+
+/**
+ * Carrega os atributos da classe Prospect
+ * @param $cpf CPF do Ciente
+ * @param $nome nome do Ciente
+ * @param $sexo sexo do Ciente
+ * @param $idade idade do Ciente
+ * @param $email email do Ciente
+ * @param $rua rua do Ciente
+ * @param $bairro bairro do Ciente
+ * @param $cidade cidade do Ciente
+ * @param $uf uf do Ciente
+ 
+ */
+
+?> 
---
+++ dim/cofig.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = 'localhost';
+$user = 'root';
+$password = '';
+
+
+?>
