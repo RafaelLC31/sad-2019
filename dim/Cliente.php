41c8e8ca8d053ab4b485877754b551cc411aea35
RafaelLC31 <rafael.lc31@aluno.ifsc.edu.br>
2019-10-11 20:14:58 -0300

    Atualizando - Implementação da ETL para Dimensão cliente

--- dim/Cliente.php
+++ dim/Cliente.php
@@ -1,79 +1,83 @@
 <?php
    namespace dimensoes;
  
-   /*
-       Model da entidade cliente
-       @author Júlio César Belenke Dos Santos
-   */
-   class Cliente{
-       /**
-       *CPF do cliente
-       *@var string
-       */
-       public $cpf;
-       /**
-       *nome do cliente
-       *@var string
-       */
-       public $nome;
-       /**
-       *Sexo do cliente
-       *@var string
-       */
-       public $sexo;
-       /**
-       *Idade do cliente
-       *@var int
-       */
-       public $idade;
-       /**
-       *Email do cliente
-       *@var string
-       */
-       public $email;
-       /**
-       *Rua do cliente
-       *@var string
-       */
-       public $rua;
-       /**
-       *Bairro do cliente
-       *@var string
-       */
-       public $bairro;
-       /**
-       *Cidade do cliente
-       *@var string
-       */
-       public $cidade;
-       /**
-       *UF do cliente
-       *@var string
-       */
-       public $uf;
+   
+/**
+* Model da entidade cliente
+* @author Paulo Roberto Córdova
+*/
+class Cliente{
+    /**
+     * CPF do cliente
+     * @var string
+     */
+    public $cpf;
+    /**
+     * Nome do Cliente
+     * @var string
+     */
+    public $nome;
+    /**
+     * Sexo do cliente
+     * @var string
+     */
+    public $sexo;
+    /**
+     * Idade do cliente
+     * @var int
+     */
+    public $idade;
+    /**
+     * Email do cliente
+     * @var string
+     */
+    public $email;
+    /**
+     * Rua do cliente
+     * @var string
+     */
+    public $rua;
+    /**
+     * Bairro do cliente
+     * @var string
+     */
+    public $bairro;
+    /**
+     * Cidade do cliente
+     * @var string
+     */
+    public $cidade;
+    /**
+     * UF do cliente
+     * @var string
+     */
+    public $uf;
   
-       /**
-       * Carrega os atributos da classe Prospect
-       * @param $cpf CPF do cliente
-       * @param $nome nome do cliente
-       * @param $sexo sexo do cliente
-       * @param $idade Idade do cliente
-       * @param $rua rua do cliente
-       * @param $bairro bairro do cliente
-       * @param $uf UF do cliente
-       * @return Void 
-       */
-      
-       public function setCliente( $cpf,$nome, $sexo, $idade, $rua, $bairro, $cidade, $uf ){
-           $this->cpf = $cpf;
-           $this->nome = $nome;
-           $this->idade = $idade;
-           $this->rua = $rua;
-           $this->bairro = $bairro;
-           $this->cidade = $cidade;
-           $this->uf = $uf;
-       }
-   }
+  
+    /**
+     * Carrega os atributos da classe Prospect
+     * @param $cpf CPF do cliente
+     * @param $nome Nome do cliente
+     * @param $sexo Sexo do cliente
+     * @param $idade Idade do cliente
+     * @param $rua Rua do cliente
+     * @param $bairo Bairro do cliente
+     * @param $cidade Cidade do cliente
+     * @param $uf UF do cliente
+     *@return Void
+     */
+    public function setCliente($cpf, $nome, $sexo, $idade, $rua, $bairro, $cidade, $uf){
+       $this->cpf = $cpf;
+       $this->nome = $nome;
+       $this->sexo = $sexo;
+       $this->idade = $idade;
+       $this->rua = $rua;
+       $this->bairro = $bairro;
+       $this->cidade = $cidade;
+       $this->uf = $uf;
+    }
+  }
+  
 ?>
  
 
