79dd956c73597f64e4c81c90ef8a056a995ec368
RafaelLC31 <rafael.lc31@aluno.ifsc.edu.br>
2019-11-01 19:09:16 -0300

    atualizando pastas

--- dim/executar.php
+++ dim/executar.php
@@ -1,14 +1,39 @@
 <?php
-   require_once('DimCliente.php');
+/*require_once('DimCliente.php');
 
-   use dimensoes\DimCliente;
+use dimensoes\DimCliente;
 
-   $dimCliente = new DimCliente();
-   try{
-      $sumCliente = $dimCliente->carregarDimCliente();
-      echo "Quantidade de Inclusões: ".$sumCliente->quantidadeInclusoes."<br>";
-      echo "Quantidade de Alterações: ".$sumCliente->quantidadeAlteracoes;
-   }catch(Exception $e){
-      die($e->getMessage());
-   }
-?>
+$dimCliente = new DimCliente();
+try{
+   $sumCliente = $dimCliente->carregarDimCliente();
+   echo "Quantidade de Inclusões: ".$sumCliente->quantidadeInclusoes."<br>";
+   echo "Quantidade de Alterações: ".$sumCliente->quantidadeAlteracoes;
+}catch(Exception $e){
+   die($e->getMessage());
+}*/
+$data = "2019-10-31";
+echo "Data: ". $data."<br>";
+echo "Dia: ".date('d', strtotime($data))."<br>";
+echo "Mês: ".date('m', strtotime($data))."<br>";
+echo "Ano: ".date('Y', strtotime($data))."<br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
