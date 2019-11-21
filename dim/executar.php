9d82052f75d301900d44fc8f1265b5d2e4dde20a
RafaelLC31 <rafael.lc31@aluno.ifsc.edu.br>
2019-11-21 10:09:29 -0300

    Atualiza Dim

--- dim/executar.php
+++ dim/executar.php
@@ -1,47 +1,41 @@
 <?php
-/*require_once('DimCliente.php');
+   require_once('DimCliente.php');
+   require_once('DimData.php');
+   require_once('DimProduto.php');
+   require_once('Sumario.php');
+   //require_once('../fatos/ETLFatosVenda.php');
 
-use dimensoes\DimCliente;
+   //use FATOS\FatosVenda;
+   use dimensoes\Sumario;
+   use dimensoes\DimData;
+   use dimensoes\DimProduto;
+   use dimensoes\DimCliente;
 
-$dimCliente = new DimCliente();
-try{
+   $dimCliente = new DimCliente();
    $sumCliente = $dimCliente->carregarDimCliente();
-   echo "Quantidade de Inclusões: ".$sumCliente->quantidadeInclusoes."<br>";
-   echo "Quantidade de Alterações: ".$sumCliente->quantidadeAlteracoes;
-}catch(Exception $e){
-   die($e->getMessage());
-}*/
-// $data = "2019-10-31";
-// echo "Data: ". $data."<br>";
-// echo "Dia: ".date('d', strtotime($data))."<br>";
-// echo "Mês: ".date('m', strtotime($data))."<br>";
-// echo "Ano: ".date('Y', strtotime($data))."<br>";
+   echo "Clientes: <br>";
+   echo "Inclusões: ".$sumCliente->quantidadeInclusoes."<br>";
+   echo "Alterações: ".$sumCliente->quantidadeAlteracoes."<br>";
+   echo "<br>==============================================<br>";
 
-require_once('DimCliente.php');
-require_once('DimData.php');
+   $dimData = new DimData();
+   $sumData = $dimData->extrairTransformarDatas();
+   echo "Datas: <br>";
+   echo "Inclusões: ".$sumData->quantidadeInclusoes."<br>";
+   echo "Alterações: ".$sumData->quantidadeAlteracoes."<br>";
+   echo "<br>==============================================<br>";
 
-$dimCliente = new DimCliente();
-$sumCliente = $dimCliente;
+   $dimProduto = new DimProduto();
+   $sumProduto = $dimProduto->carregarDimProduto();
+   echo "Produtos: <br>";
+   echo "Inclusões: ".$sumProduto->quantidadeInclusoes."<br>";
+   echo "Alterações: ".$sumProduto->quantidadeAlteracoes."<br>";
+   echo "<br>==============================================<br>";
+
+   /*$fatosVenda = new FatosVenda();
+   $sumFatos = $fatosVenda->carregarFatos('2017-01-01');
+   echo "Fatos: <br>";
+   echo "Inclusões: ".$sumFatos->quantidadeInclusoes."<br>";
+   echo "<br>==============================================<br>";*/
 
 ?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
